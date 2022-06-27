24e719af5277b4eeb1b113c8486a2fd57e6e8699
Matt F <fiaschettima@gmail.com>
2022-06-27 18:18:27 -0400

    words array

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+
+var wordOps = ["face","ton","makeup","impound","cry","tycoon","plan","experienced","jet","collection","carve",
+"current","try","carbon","tease","fat","navy","witness","prosper","legislation","mislead","crash","concede","suburb",
+"disappear","economy","latest","perfect","patient","confrontation","comedy","angle","preference","fork","seed","appreciate",
+"vessel","survivor","humanity","hook","heart","radiation","driver","difficult","harvest","confusion","bolt","adjust","deep",
+"smile"];
+
+var chosenWord = Math.floor(Math.random() * wordOps.length);
+
